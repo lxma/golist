ebe5e4588a4d721288a154ba874b44d27fb7aaca
lxma <lxma@sotalia.de>
2023-12-31 13:50:59 +0100

    Replaced Iter() by Java style iterator

--- iterator.go
+++ iterator.go
@@ -1,25 +1,80 @@
 package golist
 
-// Iter which returns a channel that delivers the elements of a list to
+import "container/list"
+
+// Iterator sets ground for a Java-Style iterator for lists.
 // permit iteration.
-//
-// Note: the list must not be modified during this iteration.
 //
 // Example:
 //
-//	lst := MakeList(1,2,3)
-//	for value := range lst.Iter() {
-//	   fmt.Print(value)
+//	iter := MakeList(1,2,3).Iterator()
+//	for iter.HasNext() {
+//	   fmt.Print(iter.Next())
 //	}
-func (l *List[T]) Iter() chan T {
-	c := make(chan T)
-	go func() {
-		elt := l.lst.Front()
-		for elt != nil {
-			c <- elt.Value.(T)
-			elt = elt.Next()
-		}
-		close(c)
-	}()
-	return c
+type Iterator[T any] interface {
+	// ForEachRemaining calls a given function for each of the remaining elements
+	ForEachRemaining(func(T))
+
+	// HasNext returns true if there is an element to read from using Next
+	HasNext() bool
+
+	// Next returns the current element and proceeds the iterator to the next element
+	Next() T
+
+	// Remove removes the last returned element from the underlying list. Example:
+	//
+	//    iter := MakeList(1, 2, 3).Iterator()
+	//    for iter.HasNext() {
+	//        if iter.Next() == 2 {
+	//            iter.Remove()
+	//        }
+	//    }
+	//
+	// leaves [1,3]
+	Remove()
+}
+
+type listIterator[T any] struct {
+	element *list.Element
+	list    *list.List
+}
+
+// Iterator creates a Java style iterator for the given list.
+// It should be used like:
+//
+//	iter := MakeList(1,2,3).Iterator()
+//	for iter.HasNext() {
+//	   fmt.Print(iter.Next())
+//	}
+func (l *List[T]) Iterator() Iterator[T] {
+	front := l.lst.Front()
+	return &listIterator[T]{front, l.lst}
+}
+
+func (iter *listIterator[T]) ForEachRemaining(f func(T)) {
+	for iter.element != nil {
+		f(iter.element.Value.(T))
+		iter.element = iter.element.Next()
+	}
+}
+
+func (iter *listIterator[T]) HasNext() bool {
+	return iter.element != nil
+}
+
+func (iter *listIterator[T]) Next() T {
+	if iter.element == nil {
+		panic("List has no next element")
+	}
+	val := iter.element.Value.(T)
+	iter.element = iter.element.Next()
+	return val
+}
+
+func (iter *listIterator[T]) Remove() {
+	if iter.element != nil {
+		iter.list.Remove(iter.element.Prev())
+	} else if iter.list.Len() > 0 {
+		iter.list.Remove(iter.list.Back())
+	}
 }
--- iterator_test.go
+++ iterator_test.go
@@ -6,17 +6,36 @@ import (
 )
 
 func TestList_Iter(t *testing.T) {
-	lst := New[int]()
+	iter := New[int]().Iterator()
 	result := make([]int, 0)
-	for value := range lst.Iter() {
-		result = append(result, value)
+	for iter.HasNext() {
+		result = append(result, iter.Next())
 	}
 	assert.Equal(t, []int{}, result)
 
-	lst = MakeList(1, 2, 3)
+	iter = MakeList(1, 2, 3).Iterator()
 	result = make([]int, 0)
-	for value := range lst.Iter() {
-		result = append(result, value)
+	for iter.HasNext() {
+		result = append(result, iter.Next())
 	}
 	assert.Equal(t, []int{1, 2, 3}, result)
+}
+
+func TestListIterator_Remove(t *testing.T) {
+	lst := MakeList(1, 2, 3, 4)
+	iter := lst.Iterator()
+	for iter.HasNext() {
+		if iter.Next() == 2 {
+			iter.Remove()
+		}
+	}
+	assert.Equal(t, []int{1, 3, 4}, lst.ToSlice())
+
+	iter = lst.Iterator()
+	for iter.HasNext() {
+		if iter.Next() == 4 {
+			iter.Remove()
+		}
+	}
+	assert.Equal(t, []int{1, 3}, lst.ToSlice())
 }
